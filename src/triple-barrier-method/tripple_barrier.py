c4c9ed7282c53b0046d0ae84cb0bfe792c84c925
Nickkontschak <32848650+Nickkontschak@users.noreply.github.com>
2023-11-01 08:42:54 +0100

    Formatting

--- src/triple-barrier-method/tripple_barrier.py
+++ src/triple-barrier-method/tripple_barrier.py
@@ -8,10 +8,13 @@ class TripleBarrierMethod:
     Attributes:
     returns (pandas.Series): Series of returns.
     n (int): Window size for the barrier method.
-    barrier (float): Barrier value to determine a label. E.g. for barrier=0.1 the label is 1 if the timeseries has a future return greater than 10% somewhere between the future 1 to n observations.
-    center (bool, optional): Center the returns by their mean to denominate an above-average return. Defaults to True.
+    barrier (float): Barrier value to determine a label. E.g. for barrier=0.1 the label is 1 if the
+                     timeseries has a future return greater than 10% somewhere between the future 1 to n observations.
+    center (bool, optional): Center the returns by their mean to denominate an above-average return.
+                             Defaults to True.
     """
-    def __init__(self, returns: pd.Series, n: int, barrier: float, center: bool = True) -> None:
+
+    def __init__(self, returns: pd.Series, n: int, barrier: float, center: bool = True):
         self.returns = returns
         self.n = n
         self.barrier = barrier
@@ -20,12 +23,12 @@ class TripleBarrierMethod:
         # Use numeric values as labels (keys) to find the minimum later.
         self.conditions = {
             # Negative barriers
-            -2: lambda x: (x <= -2*barrier),   
-            -1: lambda x: (x > -2*barrier) & (x <= -1*barrier),
+            -2: lambda x: (x <= -2 * barrier),
+            -1: lambda x: (x > -2 * barrier) & (x <= -1 * barrier),
             # Neutral: No condition needed as it will be detected if no other barrier has been hit
             # Positive barriers
-            1: lambda x: (x > 1*barrier) & (x <= 2*barrier),
-            2: lambda x: (x > 2*barrier),
+            1: lambda x: (x > 1 * barrier) & (x <= 2 * barrier),
+            2: lambda x: (x > 2 * barrier),
         }
         # Caching
         self._cumulative_returns: pd.DataFrame = None
@@ -41,7 +44,8 @@ class TripleBarrierMethod:
     @property
     def cumulative_returns(self) -> pd.DataFrame:
         """
-        Get the future cumulative returns for a range of 1 to n observations. This will be used to check which barrier has been hit first.
+        Get the future cumulative returns for a range of 1 to n observations.
+        This will be used to check which barrier has been hit first.
         Calculating if not already done.
 
         Returns:
@@ -70,16 +74,23 @@ class TripleBarrierMethod:
         """
         triple_barrier = {}
         for label, condition in self.conditions.items():
-            # Check if the condition has been met inside the rolling window of cumulative returns, columns are range(1, n+1) for the cumulative return window
+            # Check if the condition has been met inside the rolling window of cumulative returns,
+            # Columns are range(1, n+1) for the cumulative return window
             triple_barrier[label] = condition(self.cumulative_returns)
-            # For each barrier, get the first i in range(n) that crossed it. Replace False with np.nan to detect the first crossing by checking for not-missing values
+            # For each barrier, get the first i in range(n) that crossed it.
+            # Replace False with np.nan to detect the first crossing by checking for not-missing values
             # columns: [-2, -1, 1, 2] as names for the respective barriers
-            triple_barrier[label] = triple_barrier[label].replace(False, np.nan).apply(pd.Series.first_valid_index, axis=1)
+            triple_barrier[label] = (triple_barrier[label]
+                                     .replace(False, np.nan)
+                                     .apply(pd.Series.first_valid_index,axis=1)
+                                     )
         return pd.DataFrame(triple_barrier)
 
     def _remove_double_barrier_crossings(self, triple_barrier: pd.DataFrame) -> pd.DataFrame:
         """
-        Remove double barrier crossings from the barrier outcomes. This happens since to cross a 2nd (positive or negative) barrier, their respective 1st barrier needs to have been crossed before.
+        Remove double barrier crossings from the barrier outcomes. This happens since to cross
+        a 2nd (positive or negative) barrier, their respective 1st barrier needs to have been 
+        crossed before.
 
         Args:
         triple_barrier (pandas.DataFrame): Barrier crossing outcomes.
@@ -101,12 +112,13 @@ class TripleBarrierMethod:
         Returns:
         pandas.DataFrame: Identified barrier hits.
         """
-        # .idxmin() returns np.nan if no barriers have been hit => .fillna(0) labels the "no barrier hit" condition
+        # .idxmin() returns np.nan if no barriers have been hit 
+        # => .fillna(0) labels the "no barrier hit" condition
         triple_barrier = triple_barrier.idxmin(axis=1).fillna(0).astype(int)
         # Correct for a possible look-ahead-bias & errors introduced by .fillna()
-        triple_barrier.iloc[-self.n:] = np.nan  
+        triple_barrier.iloc[-self.n:] = np.nan
         return triple_barrier
-    
+
     @property
     def labels(self) -> pd.Series:
         """
@@ -118,7 +130,7 @@ class TripleBarrierMethod:
         if self._labels is None:
             self._get_labels()
         return self._labels
-    
+
     def _get_labels(self) -> pd.Series:
         """
         Execute the Triple Barrier Method.
@@ -131,7 +143,5 @@ class TripleBarrierMethod:
         labels = self._identify_barrier_hit(barriers)
         return labels
 
-
     def __repr__(self):
         return f"TripleBarrierMethod(returns, n={self.n}, barrier={self.barrier}, center={self.center})"
-    
